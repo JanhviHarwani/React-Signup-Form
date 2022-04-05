d131ffbbe989093e1e2139bf5cffe49c3161f38f
JanhviHarwani <harwanijanhvi2000@gmail.com>
2022-04-05 15:23:51 +0530

    solved filename error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./components/home";
 import SignUp from "./components/SignUp";
 import ErrorPage from "./components/ErrorPage";
 import PrivateRoute from "./components/PrivateRoute";
